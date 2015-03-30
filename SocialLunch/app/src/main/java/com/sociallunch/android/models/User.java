a31d6752e8bfd7630cac945c176c3680d794e5ad
Kenrik March <kenrikmarch@icloud.com>
2015-03-29 19:22:49 -0700

    Added profileImage to User and made it Parcelable

--- SocialLunch/app/src/main/java/com/sociallunch/android/models/User.java
+++ SocialLunch/app/src/main/java/com/sociallunch/android/models/User.java
@@ -1,5 +1,8 @@
 package com.sociallunch.android.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.firebase.client.AuthData;
 
 
@@ -8,12 +11,13 @@ import java.util.Map;
 /**
  * Created by kelei on 3/22/15.
  */
-public class User {
+public class User implements Parcelable {
     private String uid;
     private String fullName;
     private String email;
     private String foodsLiked;
     private String foodsDisliked;
+    private String profileImage;
 
     public User() {
     }
@@ -25,6 +29,7 @@ public class User {
         Map<String, Object> cachedUserProfile = (Map<String, Object>) providerData.get("cachedUserProfile");
         user.fullName = (String) cachedUserProfile.get("name");
         user.email = (String) cachedUserProfile.get("email");
+        user.profileImage = (String) cachedUserProfile.get("cover");
         return user;
     }
 
@@ -48,11 +53,48 @@ public class User {
         return foodsDisliked;
     }
 
+    public String getProfileImage() { return profileImage; }
+
     public void setFoodsLiked(String foodsLiked) {
         this.foodsLiked = foodsLiked;
     }
 
     public void setFoodsDisliked(String foodsDisliked) {
         this.foodsDisliked = foodsDisliked;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(fullName);
+        out.writeString(uid);
+        out.writeString(email);
+        out.writeString(foodsLiked);
+        out.writeString(foodsDisliked);
+        out.writeString(profileImage);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR
+            = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public User(Parcel in) {
+        this.fullName = in.readString();
+        this.uid = in.readString();
+        this.email = in.readString();
+        this.foodsLiked = in.readString();
+        this.foodsDisliked = in.readString();
+        this.profileImage = in.readString();
     }
 }

79cdd04a4ab59ebe7bb77a85143c281e8413f638
Sebastian Ng <sebng85@gmail.com>
2015-04-05 21:40:12 +0800

    Auto-indent.

--- SocialLunch/app/src/main/java/com/sociallunch/android/net/YelpAPI.java
+++ SocialLunch/app/src/main/java/com/sociallunch/android/net/YelpAPI.java
@@ -21,90 +21,90 @@ import org.scribe.oauth.OAuthService;
  *
  */
 public class YelpAPI {
-  /*
-   * Update OAuth credentials below from the Yelp Developers API site:
-   * http://www.yelp.com/developers/getting_started/api_access
-   */
+    /*
+     * Update OAuth credentials below from the Yelp Developers API site:
+     * http://www.yelp.com/developers/getting_started/api_access
+     */
 
-  public static final String DEFAULT_TERM = "Restaurants";
-  public static final String DEFAULT_LOCATION = "San Francisco, CA";
-  public static final int DEFAULT_LIMIT = 10;
-  private static final String API_HOST = "api.yelp.com";
-  private static final String SEARCH_PATH = "/v2/search";
-  private static final String BUSINESS_PATH = "/v2/business";
+    public static final String DEFAULT_TERM = "Restaurants";
+    public static final String DEFAULT_LOCATION = "San Francisco, CA";
+    public static final int DEFAULT_LIMIT = 10;
+    private static final String API_HOST = "api.yelp.com";
+    private static final String SEARCH_PATH = "/v2/search";
+    private static final String BUSINESS_PATH = "/v2/business";
 
-  OAuthService service;
-  Token accessToken;
+    OAuthService service;
+    Token accessToken;
 
-  /**
-   * Setup the Yelp API OAuth credentials.
-   *
-   * @param consumerKey Consumer key
-   * @param consumerSecret Consumer secret
-   * @param token Token
-   * @param tokenSecret Token secret
-   */
-  public YelpAPI(String consumerKey, String consumerSecret, String token, String tokenSecret) {
-    this.service =
-        new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(consumerKey)
-            .apiSecret(consumerSecret).build();
-    this.accessToken = new Token(token, tokenSecret);
-  }
+    /**
+     * Setup the Yelp API OAuth credentials.
+     *
+     * @param consumerKey    Consumer key
+     * @param consumerSecret Consumer secret
+     * @param token          Token
+     * @param tokenSecret    Token secret
+     */
+    public YelpAPI(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.service =
+                new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(consumerKey)
+                        .apiSecret(consumerSecret).build();
+        this.accessToken = new Token(token, tokenSecret);
+    }
 
-  /**
-   * Creates and sends a request to the Search API by term and location.
-   * <p>
-   * See <a href="http://www.yelp.com/developers/documentation/v2/search_api">Yelp Search API V2</a>
-   * for more info.
-   *
-   * @param term <tt>String</tt> of the search term to be queried
-   * @param location <tt>String</tt> of the location
-   * @return <tt>String</tt> JSON Response
-   */
-  public String searchForBusinessesByLocation(String term, String location, int offset, int limit) {
-    OAuthRequest request = createOAuthRequest(SEARCH_PATH);
-    request.addQuerystringParameter("term", term);
-    request.addQuerystringParameter("location", location);
-    request.addQuerystringParameter("offset", String.valueOf(offset));
-    request.addQuerystringParameter("limit", String.valueOf(limit));
-    return sendRequestAndGetResponse(request);
-  }
+    /**
+     * Creates and sends a request to the Search API by term and location.
+     * <p/>
+     * See <a href="http://www.yelp.com/developers/documentation/v2/search_api">Yelp Search API V2</a>
+     * for more info.
+     *
+     * @param term     <tt>String</tt> of the search term to be queried
+     * @param location <tt>String</tt> of the location
+     * @return <tt>String</tt> JSON Response
+     */
+    public String searchForBusinessesByLocation(String term, String location, int offset, int limit) {
+        OAuthRequest request = createOAuthRequest(SEARCH_PATH);
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("location", location);
+        request.addQuerystringParameter("offset", String.valueOf(offset));
+        request.addQuerystringParameter("limit", String.valueOf(limit));
+        return sendRequestAndGetResponse(request);
+    }
 
-  /**
-   * Creates and sends a request to the Business API by business ID.
-   * <p>
-   * See <a href="http://www.yelp.com/developers/documentation/v2/business">Yelp Business API V2</a>
-   * for more info.
-   *
-   * @param businessID <tt>String</tt> business ID of the requested business
-   * @return <tt>String</tt> JSON Response
-   */
-  public String searchByBusinessId(String businessID) {
-    OAuthRequest request = createOAuthRequest(BUSINESS_PATH + "/" + businessID);
-    return sendRequestAndGetResponse(request);
-  }
+    /**
+     * Creates and sends a request to the Business API by business ID.
+     * <p/>
+     * See <a href="http://www.yelp.com/developers/documentation/v2/business">Yelp Business API V2</a>
+     * for more info.
+     *
+     * @param businessID <tt>String</tt> business ID of the requested business
+     * @return <tt>String</tt> JSON Response
+     */
+    public String searchByBusinessId(String businessID) {
+        OAuthRequest request = createOAuthRequest(BUSINESS_PATH + "/" + businessID);
+        return sendRequestAndGetResponse(request);
+    }
 
-  /**
-   * Creates and returns an {@link org.scribe.model.OAuthRequest} based on the API endpoint specified.
-   *
-   * @param path API endpoint to be queried
-   * @return <tt>OAuthRequest</tt>
-   */
-  private OAuthRequest createOAuthRequest(String path) {
-    OAuthRequest request = new OAuthRequest(Verb.GET, "http://" + API_HOST + path);
-    return request;
-  }
+    /**
+     * Creates and returns an {@link org.scribe.model.OAuthRequest} based on the API endpoint specified.
+     *
+     * @param path API endpoint to be queried
+     * @return <tt>OAuthRequest</tt>
+     */
+    private OAuthRequest createOAuthRequest(String path) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, "http://" + API_HOST + path);
+        return request;
+    }
 
-  /**
-   * Sends an {@link org.scribe.model.OAuthRequest} and returns the {@link org.scribe.model.Response} body.
-   *
-   * @param request {@link org.scribe.model.OAuthRequest} corresponding to the API request
-   * @return <tt>String</tt> body of API response
-   */
-  private String sendRequestAndGetResponse(OAuthRequest request) {
-    Log.d("com.sociallunch.android", "Querying " + request.getCompleteUrl() + " ...");
-    this.service.signRequest(this.accessToken, request);
-    Response response = request.send();
-    return response.getBody();
-  }
+    /**
+     * Sends an {@link org.scribe.model.OAuthRequest} and returns the {@link org.scribe.model.Response} body.
+     *
+     * @param request {@link org.scribe.model.OAuthRequest} corresponding to the API request
+     * @return <tt>String</tt> body of API response
+     */
+    private String sendRequestAndGetResponse(OAuthRequest request) {
+        Log.d("com.sociallunch.android", "Querying " + request.getCompleteUrl() + " ...");
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
 }
